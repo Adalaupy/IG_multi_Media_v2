9321004184fbd14d43db86937d614720757810f0
Adalaupy <129365764+Adalaupy@users.noreply.github.com>
2024-01-23 22:14:37 +0800

    Update API.py

--- API.py
+++ API.py
@@ -35,7 +35,7 @@ print(origins)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins= ["*"]     #origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
